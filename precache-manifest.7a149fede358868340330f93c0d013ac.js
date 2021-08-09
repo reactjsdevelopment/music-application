433fafda5706b1f6bdac0f8e3fbf71fea6819c4a
DESKTOP-1ASQ7K6\sunny <sunny.gupta@docquity.com>
2021-08-09 11:25:38 +0530

    Updates

---
+++ precache-manifest.7a149fede358868340330f93c0d013ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7510a2b6e639645a231ca281f5988a5",
+    "url": "/music-application/index.html"
+  },
+  {
+    "revision": "32185d3551329f6752b6",
+    "url": "/music-application/static/css/2.46b578d6.chunk.css"
+  },
+  {
+    "revision": "5116bd5450c861c98286",
+    "url": "/music-application/static/css/main.a739bc11.chunk.css"
+  },
+  {
+    "revision": "32185d3551329f6752b6",
+    "url": "/music-application/static/js/2.dcc87960.chunk.js"
+  },
+  {
+    "revision": "0def066106b5692cd5c135dcafc5767d",
+    "url": "/music-application/static/js/2.dcc87960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5116bd5450c861c98286",
+    "url": "/music-application/static/js/main.46421190.chunk.js"
+  },
+  {
+    "revision": "bf9e692436cdecbe6de0",
+    "url": "/music-application/static/js/runtime-main.293c891d.js"
+  },
+  {
+    "revision": "1b2651924062685b5a98dbdb6331a52b",
+    "url": "/music-application/static/media/music.1b265192.jpeg"
+  }
+]);
