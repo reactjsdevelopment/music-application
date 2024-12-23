a510eeba093c87017e6733bf907226a4f9a6d04b
Gupta, Sunny <sunnyvales789@gmail.com>
2024-12-24 00:32:22 +0530

    update

--- src/components/lodash.jsx
+++ src/components/lodash.jsx
@@ -1,3 +1,17 @@
+// Lodash is one such library that is the successor of underscore.js. It is used to simplify your work of managing and editing objects and arrays by providing lots of utility or service methods to do things quickly.
+
+// There is another library to optimize your code is Ramda. It is a functional library like underscore.js, Lodash.js.
+
+// Some of the methods which I found quite handy and ease my the development process is _pick, _map, _filter, _has,  cloneDeep.
+
+// Setup-
+
+// npm i lodash
+// npm i @types/lodash
+import * as _ from "lodash";
+
+// For more information, you can check the official website.
+
 let arrData = [
     {
       a: 1,
